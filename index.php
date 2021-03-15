d6db94c6724afe5e7e2722308a83258e39175926
MiaS-Git <55926870+MiaS-Git@users.noreply.github.com>
2021-03-15 17:19:14 -0400

    Add files via upload
    Added intro page/Login screen

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Jeopardy</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Squada+One&display=swap" rel="stylesheet">
+       <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+        <div class="main">
+            <h1>JEOPARDY!</h1>
+
+        <div class="border">
+            <div class="text">
+                <br>
+                <center>Team</center>
+                <ul style="font-size:18px">
+                <li style="color:#10000f">Team</li>
+                    <li style="color:#0a000a">Leader: Tanjima</li>
+                    <li style="color:#070007">JeoPHardy</li>
+                    <li style="color:#030003">This is a recreation of the gameshow Jeopardy! using php and introducing a login feature.</li>
+                    <li style="color:#050005">Mia Stroud (Intro/login), Saadh Ahmed/Anish Ganga (game screen), Tanjina (end page/youtube)</li>
+                </ul>
+
+            </div>
+            <a href="login.php">
+                <span class="btn white">Login</span>
+            </a>
+        </div>
+    </body>
+</html>
