4e5a299fa561650be0add785455cca24e1409d29
Saadh Ahmed <44828093+saadhahmed00@users.noreply.github.com>
2021-03-19 12:12:16 -0400

    post user_code to results

--- game-question.php
+++ game-question.php
@@ -14,12 +14,14 @@
 
         if(isset($_POST['submit_answer'])){
 			$game_answer = $_POST['uanswer'];
+            $user_code = $_POST['user_code'];
 		}
     ?>
 </body>
         <form action="results.php" method="post">
             <strong><label for="uanswer"><?=$questions[$data];?></label></strong>
             <input type="text" name="uanswer"><br>
+            <input type="hidden" name="user_code" value=<?=$data;?>>
             <input type="submit" name="submit_answer" value="Submit">
         </form>
 </html>
