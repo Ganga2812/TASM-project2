aa9de4abee301bf521e15334428a41d6ddad3898
Saadh Ahmed <44828093+saadhahmed00@users.noreply.github.com>
2021-03-19 11:58:31 -0400

    added game post logic

---
+++ game-data.php
@@ -0,0 +1,33 @@
+<?php
+    // Game questions and answers
+    
+    $questions = [
+        "one_1"  => "It occupies about half the land mass of the South American continent",
+        "one_2" => "The mafic type of this 5-letter underground molten rock can reach more than 2000 degrees Fahrenheit",
+        "one_3" => "To calculate this for a circle multiply pi by the square of the radius",
+        "one_4"  => "This home city to Kansas State University shares its name with a Big Apple borough",
+        "two_1" => "Martinique is one of its overseas departments",
+        "two_2" => "In 1995 physicist Melissa Franklin was part of the team that discovered the elusive top one of these",
+        "two_3" => "It's a unit of output for a brainstorming session",
+        "two_4" => "No relation to the bubbly this Illinois city is often paired with Urbana",
+        "three_1" => "King Abdullah II is the head of state of this country that borders Israel",
+        "three_2" => "The water on the road mirage is due to hotter air above the blacktop causing this bending of light rays",
+        "three_3" => "It's the only Greek letter that fits the category",
+        "three_4" => "This Midwestern city whose name begins with the state it's in is home to the NCAA"
+    ];
+
+    $answers = [
+        "one_1"  => "Brazil",
+        "one_2" => "Magma",
+        "one_3" => "Area",
+        "one_4"  => "Manhattan",
+        "two_1" => "France",
+        "two_2" => "Quark",
+        "two_3" => "Idea",
+        "two_4" => "Champaign",
+        "three_1" => "Jordan",
+        "three_2" => "Refraction",
+        "three_3" => "Iota",
+        "three_4" => "Indianapolis"
+    ];
+?>
