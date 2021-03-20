00a9618e237955d22b0784cccef3f30007fa0925
Saadh Ahmed <44828093+saadhahmed00@users.noreply.github.com>
2021-03-20 01:20:50 -0400

    added some comments

--- results.php
+++ results.php
@@ -1,1 +1,25 @@
-<?php echo $_POST['user_code']; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Check if it's the correct answer
+    // if 
+        // true
+        // html code that says it's right
+        // += score amount
+
+        // false
+        // html code that says it was wrong
+
+        // back to questions button
+
+        
+    ?>
+</body>
+</html>
